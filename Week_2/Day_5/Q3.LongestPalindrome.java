5fdcf6c39e8b8b58135fdf31337e1c9795dfd4cd
hemani <hemanimajhi189@gmail.com>
2026-06-12 18:11:03 +0530

    Week 2 Solutions

---
+++ Week_2/Day_5/Q3.LongestPalindrome.java
@@ -0,0 +1,42 @@
+class Solution {
+    public String longestPalindrome(String s) {
+        int start = 0;
+        int maxLen = 1;
+
+        for (int i = 0; i < s.length(); i++) {
+
+      
+            int left = i, right = i;
+
+            while (left >= 0 && right < s.length()
+                    && s.charAt(left) == s.charAt(right)) {
+
+                if (right - left + 1 > maxLen) {
+                    start = left;
+                    maxLen = right - left + 1;
+                }
+
+                left--;
+                right++;
+            }
+
+          
+            left = i;
+            right = i + 1;
+
+            while (left >= 0 && right < s.length()
+                    && s.charAt(left) == s.charAt(right)) {
+
+                if (right - left + 1 > maxLen) {
+                    start = left;
+                    maxLen = right - left + 1;
+                }
+
+                left--;
+                right++;
+            }
+        }
+
+        return s.substring(start, start + maxLen);
+    }
+}
