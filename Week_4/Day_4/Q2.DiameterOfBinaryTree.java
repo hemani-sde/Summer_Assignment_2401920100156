41cfff2213ada5823e01018bb745b6d583625e1f
hemani <hemanimajhi189@gmail.com>
2026-06-25 16:36:35 +0530

    Week 4 Solutions

---
+++ Week_4/Day_4/Q2.DiameterOfBinaryTree.java
@@ -0,0 +1,21 @@
+class Solution {
+    private int diameter = 0;
+
+    private int height(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int leftHeight = height(root.left);
+        int rightHeight = height(root.right);
+
+        diameter = Math.max(diameter, leftHeight + rightHeight);
+
+        return 1 + Math.max(leftHeight, rightHeight);
+    }
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        height(root);
+        return diameter;
+    }
+}
